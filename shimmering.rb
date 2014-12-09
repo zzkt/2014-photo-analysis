bf9e232ab3ce928cb62b70dd6c55e1d6ce6d3c24
nik gaffney <nik@fo.am>
2014-12-09 16:26:58 +0100

    insular

--- shimmering.rb
+++ shimmering.rb
@@ -1,22 +1,23 @@
-# -*- mode: enh-ruby encoding: utf-8 -*-
+# -*- mode: enh-ruby; coding: utf-8; -*-
 #
 # Image and metadata analysis for photoset 02014 (365)
 #
 # Copyright (C) 2014 FoAM vzw
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 #
 # Authors
 #  - nik gaffney <nik@fo.am>
